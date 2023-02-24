ef91be1f7a002f8d3e10eb263ea72398a60b4387
sourav1-2026 <souravkshaw2019@gmail.com>
2023-02-24 21:00:02 +0530

    web scraper done

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 import logging
+import pymongo
+
 logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
 
 app = Flask(__name__)
@@ -72,6 +74,15 @@ def index():
                           "Comment": custComment}
                 reviews.append(mydict)
             logging.info("log my final result {}".format(reviews))
+            
+            client = pymongo.MongoClient("mongodb+srv://shawk:sourav2023@cluster0.u0rfhuq.mongodb.net/?retryWrites=true&w=majority")
+            # making database
+            db = client["review_scrap"]
+            # make collections
+            review_collectons=db['review_scrap_data']
+            review_collectons.insert_many(reviews)
+
+
             return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
         except Exception as e:
             logging.info(e)
